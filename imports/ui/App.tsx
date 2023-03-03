66ee273d5a2007b51cb286beca67716fff5fce5e
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2023-03-02 23:52:02 -0600

    initial ui

---
+++ imports/ui/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+export const App = () => (
+  <ChakraProvider>
+    <h1>Welcome to Meteor!</h1>
+  </ChakraProvider>
+);
